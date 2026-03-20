87e4b1f29703ad773968fff5d866b25042c2763f
Wilmer <395robotics@gmail.com>
2026-03-20 12:27:33 -0400

    can id change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,16 +46,17 @@ public final class Constants {
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 1;
-    public static final int kRearLeftDrivingCanId = 5;
-    public static final int kFrontRightDrivingCanId = 3;
+    public static final int kFrontLeftDrivingCanId = 20; 
+    public static final int kFrontRightDrivingCanId = 22; 
+    public static final int kRearLeftDrivingCanId = 24; 
     public static final int kRearRightDrivingCanId = 7;
 
-    public static final int kFrontLeftTurningCanId = 2;
-    public static final int kRearLeftTurningCanId = 6;
-    public static final int kFrontRightTurningCanId = 4;
-    public static final int kRearRightTurningCanId = 8;
+  
 
+    public static final int kFrontLeftTurningCanId = 21; 
+    public static final int kFrontRightTurningCanId = 23; 
+    public static final int kRearLeftTurningCanId = 25; 
+    public static final int kRearRightTurningCanId = 8; 
     public static final int kLeftShooterCanId = 9;
     public static final int kRightShooterCanId = 10;
 
